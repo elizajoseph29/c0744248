a9fb07dcc6dc58dcbfc08ede0c2f84a07b9a1a8a
visualstudio <visualstudio@230-PC01>
2019-02-07 11:44:33 -0500

    Javascript functions

--- c0744248/js/main.js
+++ c0744248/js/main.js
@@ -1,1 +1,12 @@
-﻿// Your code here!
+﻿function myFunction(a, b) {
+    //var a = 5;
+    //var b = 6;
+    return a + b;
+}
+
+
+function arrays() {
+    var pens;
+    pens = ["Red", "Orange", "Green", "Blue", "Purple"];
+    console.log(pens);
+}
